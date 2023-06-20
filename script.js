3b7dc77c72617877133d9124c08cf2173c730f8d
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-06-20 13:55:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ document.getElementById("toggleDarkMode").addEventListener("click", function()
     const body = document.querySelector("body");
     body.classList.toggle("dark-mode");
 });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhYaLPuhkEpjDAS8oAC1OoR9blnNL8ydo",
+    authDomain: "note-taking-website-5f173.firebaseapp.com",
+    databaseURL: "https://note-taking-website-5f173-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "note-taking-website-5f173",
+    storageBucket: "note-taking-website-5f173.appspot.com",
+    messagingSenderId: "79500910698",
+    appId: "1:79500910698:web:c3b4806a9eed1464b66a13",
+    measurementId: "G-01DX3JT797"
+  };
